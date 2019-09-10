3f6458fb1e9f302b3852618c27371333cf4647ac
Pavel Kononov <woodsleaf@gmail.com>
2019-09-10 11:47:49 +0300

    Create makeexe_cmd.py

---
+++ makeexe_cmd.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+'''python makeexe_cmd.py build'''
+name = 'matrix'
+filename = name + '.py'
+from cx_Freeze import setup, Executable
+
+import sys
+'''
+import argparse
+parser = argparse.ArgumentParser(description='Enter filename')
+parser.add_argument(
+    '-f',
+    '--filename',
+    default='example',
+    help='provide an filename with out extension -f=filename (default: example )'
+)
+my_namespace = parser.parse_args()
+print(my_namespace)
+name = my_namespace.filename
+filename = my_namespace.filename + '.py'
+'''
+
+base = base = 'Console'
+
+executables = [Executable(filename,
+                          targetName=name + '_wx.exe',
+                          base=base,
+                          icon='example.ico')]
+
+#buildOptions = dict(packages = [], excludes = [])
+excludes = ['logging', 'unittest', 'email', 'html', 'http', 'urllib', 'xml',
+            'bz2', 'select']
+
+zip_include_packages = ['collections', 'encodings', 'importlib']  # , 'wx'
+
+# options = dict(build_exe = buildOptions),
+options = {
+    'build_exe': {
+        'include_msvcr': True,
+        'excludes': excludes,
+        'zip_include_packages': zip_include_packages,
+        'build_exe': 'build_windows',
+    }
+}
+
+setup(name=name,
+      version='0.0.13',
+      description='My {} App!'.format(name),
+      options=options,
+      executables=executables)  # options = dict(build_exe = buildOptions),
